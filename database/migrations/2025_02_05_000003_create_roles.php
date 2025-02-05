e644b97e70a33d9add46f9d80069346f74089bab
bulat <shigabutdinovbr@gmail.com>
2025-02-05 18:19:43 +0300

    Update roles migration

--- database/migrations/2025_02_05_000003_create_roles.php
+++ database/migrations/2025_02_05_000003_create_roles.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique()->comment('Название роли');
             $table->string('slug')->unique()->comment('Уникальный идентификатор');
-            $table->timestamps();
         });
     }
 
