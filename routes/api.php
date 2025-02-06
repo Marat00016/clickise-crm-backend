4889527e43590f1608e98903879525c78491c7a1
bulat <shigabutdinovbr@gmail.com>
2025-02-06 14:26:14 +0300

    Initial Laravel JSON:API

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,22 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
+use LaravelJsonApi\Laravel\Http\Controllers\JsonApiController;
+use LaravelJsonApi\Laravel\Routing\ResourceRegistrar;
+use LaravelJsonApi\Laravel\Routing\Relationships;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/contact', [\App\Http\Controllers\ContactController::class, 'create']);
+JsonApiRoute::server('v1')->prefix('v1')->resources(function (ResourceRegistrar $server) {
+    $server->resource('contacts', JsonApiController::class)
+        ->readOnly()
+        ->relationships(function (Relationships $relations) {
+            $relations->hasOne('clients')->readOnly();
+//            $relations->hasMany('comments')->readOnly();
+//            $relations->hasMany('tags')->readOnly();
+        });
+    $server->resource('clients', JsonApiController::class)->readOnly();
+});
