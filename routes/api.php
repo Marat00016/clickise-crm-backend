49c486d56ebc41a98840d859e77c5b1f96146afc
shigabutdinoff <shigabutdinovbr@gmail.com>
2025-02-07 15:55:04 +0300

    Add JSON Web Token Authentication for Laravel

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use LaravelJsonApi\Laravel\Facades\JsonApiRoute;
@@ -9,7 +10,11 @@ use LaravelJsonApi\Laravel\Routing\Relationships;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+//})->middleware('auth:sanctum');
+})->middleware('auth:api');
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 JsonApiRoute::server('v1')->prefix('v1')->resources(function (ResourceRegistrar $server) {
     $server->resource('contacts', JsonApiController::class);
