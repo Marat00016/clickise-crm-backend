56ba43a0d7bd0a2eabb0b71626100950a9886559
bulat <shigabutdinovbr@gmail.com>
2025-02-13 13:10:55 +0300

    Attaching dialogs_contacts in WebhookController

--- app/Http/Controllers/Api/V1/WebhookController.php
+++ app/Http/Controllers/Api/V1/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Bot;
 use App\Models\Contact;
+use App\Models\Dialog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use LaravelJsonApi\Laravel\Http\Controllers\Actions;
@@ -22,14 +23,50 @@ class WebhookController extends Controller
     use Actions\AttachRelationship;
     use Actions\DetachRelationship;
 
-    public function handle(Request $request, $token)
+    const api = 'https://api.telegram.org';
+
+    public function rules(): array
+    {
+        return [
+            'update_id' => ['required', 'integer'],
+            'message' => ['array'],
+            'message.message_id' => ['required', 'integer'],
+            'message.from' => ['array'],
+            'message.from.id' => ['required', 'integer'],
+            'message.from.is_bot' => ['required', 'boolean'],
+            'message.from.first_name' => ['required', 'string'],
+            'message.from.last_name' => ['string'],
+            'message.from.username' => ['string'],
+            'message.from.language_code' => ['string', 'size:2'],
+            'message.chat' => ['required', 'array'],
+            'message.chat.id' => ['required', 'integer'],
+            'message.chat.first_name' => ['string'],
+            'message.chat.last_name' => ['string'],
+            'message.chat.username' => ['string'],
+            'message.chat.type' => ['required', 'string', 'in:private,group,supergroup,channel'],
+            'message.date' => ['required', 'integer'],
+            'message.text' => ['string'],
+        ];
+    }
+
+    public function handle(Request $request, string $token)
     {
         $bot = Bot::where('webhook_token', $token)->firstOrFail();
-        $contact = Contact::firstOrFail();
 
-        return Http::post(sprintf('https://api.telegram.org/bot%1$s/sendMessage', $bot->token), [
-            'chat_id' => $contact->chat_id,
-            'text' => json_encode($request->getContent(), JSON_UNESCAPED_UNICODE),
+        $validated = $request->validate($this->rules());
+
+        if (data_get($validated, 'message.from.id')) {
+            $dialog = Dialog::firstOrCreate([
+                'chat_id' => data_get($validated, 'message.chat.id'),
+                'bot_id' => $bot->id,
+            ]);
+            $contact = Contact::firstOrCreate(['chat_id' => data_get($validated, 'message.from.id')]);
+            $dialog->contacts()->syncWithoutDetaching([$contact->uuid]);
+        }
+
+        return Http::post(sprintf('%1$s/bot%2$s/sendMessage', self::api, $bot->token), [
+            'chat_id' => data_get($validated, 'message.chat.id'),
+            'text' => $request->collect()->toJson(JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE),
         ])->json();
     }
 }
